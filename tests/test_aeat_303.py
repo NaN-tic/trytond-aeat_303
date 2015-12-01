8dd455f8552e5e9eaaabd9abae5c251487de7791
resteve <resteve@zikzakmedia.com>
2015-12-01 11:02:44 +0100

    Tests

--- tests/test_aeat_303.py
+++ tests/test_aeat_303.py
@@ -1,28 +1,18 @@
-#!/usr/bin/env python
+# This file is part of the aeat_303 module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class Aeat303TestCase(unittest.TestCase):
-    'Test AEAT 303 module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('aeat_303')
-
-    def test0005views(self):
-        'Test views'
-        test_view('aeat_303')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class Aeat303TestCase(ModuleTestCase):
+    'Test Aeat 303 module'
+    module = 'aeat_303'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            Aeat303TestCase))
+        Aeat303TestCase))
     return suite
