5130b7640244af9107e7d80eaafe3a5b662bb59d
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-09-07 09:47:50 +0200

    Correctly manage string and bytes for Python3 support

--- aeat.py
+++ aeat.py
@@ -3,6 +3,7 @@ from decimal import Decimal
 import datetime
 import calendar
 import unicodedata
+import sys
 
 from retrofix import aeat303
 from retrofix.record import Record, write as retrofix_write
@@ -29,7 +30,9 @@ _Z = Decimal("0.0")
 
 
 def remove_accents(unicode_string):
-    if isinstance(unicode_string, str):
+    str_ = str if sys.version_info < (3, 0) else bytes
+    unicode_ = unicode if sys.version_info < (3, 0) else str
+    if isinstance(unicode_string, str_):
         unicode_string_bak = unicode_string
         try:
             unicode_string = unicode_string_bak.decode('iso-8859-1')
@@ -39,7 +42,7 @@ def remove_accents(unicode_string):
             except UnicodeDecodeError:
                 return unicode_string_bak
 
-    if not isinstance(unicode_string, unicode):
+    if not isinstance(unicode_string, unicode_):
         return unicode_string
 
     unicode_string_nfd = ''.join(
@@ -828,5 +831,5 @@ class Report(Workflow, ModelSQL, ModelView):
         data = remove_accents(data).upper()
         if isinstance(data, unicode):
             data = data.encode('iso-8859-1')
-        self.file_ = bytes(data)
+        self.file_ = self.__class__.file_.cast(data)
         self.save()
