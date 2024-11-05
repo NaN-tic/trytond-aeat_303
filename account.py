b497c3f4fc36813a5680d69d8b9ffbfbe39ee7ca
Bernat Brunet <bernat@nan-tic.com>
2024-11-05 16:45:47 +0100

    When set to draft the 303 module, if a move is calcualted, remove it, Not save an historize, it's not necessary for this type of account move.

--- account.py
+++ account.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from trytond.pool import PoolMeta
+from trytond.pool import Pool, PoolMeta
 
 
 class Move(metaclass=PoolMeta):
@@ -8,3 +8,12 @@ class Move(metaclass=PoolMeta):
     @classmethod
     def _get_origin(cls):
         return super(Move, cls)._get_origin() + ['aeat.303.report']
+
+    def get_allow_draft(self, name):
+        pool = Pool()
+        Report = pool.get('aeat.303.report')
+
+        result = super().get_allow_draft(name)
+        if self.origin and isinstance(self.origin, Report):
+            return True
+        return result
